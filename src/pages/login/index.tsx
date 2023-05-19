2674d345c72c3b0a080f4b2a90209a2620c4fb17
Joao Souza <joaoosouza07@gmail.com>
2023-05-18 23:59:07 -0300

    :sparkles: feat: Login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,71 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Flex, Text, Center, Input, Button } from '@chakra-ui/react';
+
+import logo from '../../../public/images/logo.svg';
+
+export default function Login() {
+    return (
+        <>
+            <Head>
+                <title>BarberPRO - Login</title>
+            </Head>
+
+            <Flex background='barber.900' height='100vh' alignItems='center' justifyContent='center'>
+                
+                <Flex width={640} direction='column' p={12} rounded={3}>
+                    <Center p={4}>
+                        <Image 
+                            src={logo}
+                            quality={100}
+                            width={240}
+                            alt='Logo BarberPRO'
+                        />
+                    </Center>
+
+                    <Input 
+                        background='barber.400'
+                        rounded={3}
+                        variant='filled'
+                        size='lg'
+                        placeholder='Digite seu e-mail...'
+                        type='email'
+                        mb={3}
+                        color='button.gray'
+                    />
+
+                    <Input 
+                        background='barber.400'
+                        rounded={3}
+                        variant='filled'
+                        size='lg'
+                        placeholder='Digite sua senha...'
+                        type='text'
+                        mb={5}
+                        color='button.gray'
+                    />
+
+                    <Button
+                        background='button.cta'
+                        mb={5}
+                        color='gray.900'
+                        size='lg'
+                        _hover={{ background: '#ffb13e' }}
+                    >
+                        Acessar
+                    </Button>
+
+                    <Center mt={2}>
+                        <Link href='/register'>
+                            <Text color='button.default' cursor='pointer'>Não possui uma conta? <strong>Cadastre-se</strong></Text>
+                        </Link>
+                    </Center>
+
+                </Flex>
+
+            </Flex>
+
+        </>
+    );
+}
