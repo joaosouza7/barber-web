371e58700f8e80982fd779ff2d6b16a40b5e803a
Joao Souza <joaoosouza07@gmail.com>
2023-05-18 23:17:37 -0300

    :tada: Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BarberPRO</title>
+      </Head>
+      
+      <div>
+        <h2>HOME</h2>
+      </div>
+    </>
+  );
+}
